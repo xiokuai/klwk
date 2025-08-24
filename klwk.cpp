ef4ff0d76f5dbc25529378abe66f7bcf1a6d9ecf
xiaokuai <163656492+xiokuai@users.noreply.github.com>
2025-08-24 19:43:41 +0800

    重构整体代码
    重构整体代码以增加可读性

--- klwk.cpp
+++ klwk.cpp
@@ -4,146 +4,210 @@
 #include <windows.h>
 #include <io.h>
 #include <fcntl.h>
+#include <vector>
+#include <string>
 using namespace std;
 
+// 游戏常量定义
+const int MAP_SIZE = 7;
+const int PLAYER = 1;
+const int GOAL = 2;
+const int WALL = 3;
+const int TRAP = 4;
+const int DOOR = 5;
+const int EMPTY = 0;
+
+// 游戏状态结构
+struct GameState {
+    int map[MAP_SIZE][MAP_SIZE];
+    int playerX, playerY;
+    int goalX, goalY;
+    vector<pair<int, int>> traps;
+    int level;
+};
+
+// 函数声明
+void initializeConsole();
+void initializeGame(GameState& game, int level);
+void generateTraps(GameState& game);
+void printMap(const GameState& game);
+void processInput(GameState& game, char input);
+bool checkGameStatus(const GameState& game);
+void clearScreen();
+
 int main() {
-    // 设置控制台为UTF-8模式
+    initializeConsole();
+    srand(time(0));
+    
+    for(int level = 1; ; level++) {
+        GameState game;
+        initializeGame(game, level);
+        
+        while(true) {
+            printMap(game);
+            
+            if(!checkGameStatus(game)) {
+                break;
+            }
+            
+            char input;
+            cout << "请进行移动 ";
+            cin >> input;
+            
+            processInput(game, input);
+            clearScreen();
+        }
+        
+        clearScreen();
+    }
+    
+    return 0;
+}
+
+void initializeConsole() {
     SetConsoleOutputCP(CP_UTF8);
     SetConsoleCP(CP_UTF8);
-    for(int gs=1;;gs++) {
-        int n[7][7] = {{3,5,3,3,3,3,3},
-                       {5,1,0,0,0,0,3},
-                       {3,0,0,0,0,0,3},
-                       {3,0,0,0,0,0,3},
-                       {3,0,0,0,0,0,3},
-                       {3,0,0,0,0,2,3},
-                       {3,3,3,3,3,3,3}};
-        int a=1, b=1;
-        int ah, bh;
-        srand(time(0));
-        ah = rand()%5+1;
-        if(ah==1) {
-            bh = rand()%4+2;
-        } else if(ah==5) {
-            bh = rand()%4+1;
+}
+
+void initializeGame(GameState& game, int level) {
+    // 初始化基础地图
+    int baseMap[MAP_SIZE][MAP_SIZE] = {
+        {3,5,3,3,3,3,3},
+        {5,1,0,0,0,0,3},
+        {3,0,0,0,0,0,3},
+        {3,0,0,0,0,0,3},
+        {3,0,0,0,0,0,3},
+        {3,0,0,0,0,2,3},
+        {3,3,3,3,3,3,3}
+    };
+    
+    // 复制基础地图
+    for(int i = 0; i < MAP_SIZE; i++) {
+        for(int j = 0; j < MAP_SIZE; j++) {
+            game.map[i][j] = baseMap[i][j];
+            if(game.map[i][j] == PLAYER) {
+                game.playerX = i;
+                game.playerY = j;
+            } else if(game.map[i][j] == GOAL) {
+                game.goalX = i;
+                game.goalY = j;
+            }
+        }
+    }
+    
+    game.level = level;
+    game.traps.clear();
+    generateTraps(game);
+}
+
+void generateTraps(GameState& game) {
+    // 生成第一个陷阱
+    int trap1X = rand() % 5 + 1;
+    int trap1Y;
+    if(trap1X == 1) {
+        trap1Y = rand() % 4 + 2;
+    } else if(trap1X == 5) {
+        trap1Y = rand() % 4 + 1;
+    } else {
+        trap1Y = rand() % 5 + 1;
+    }
+    game.map[trap1X][trap1Y] = TRAP;
+    game.traps.push_back({trap1X, trap1Y});
+    
+    // 生成第二个陷阱（确保不与第一个重合）
+    int trap2X, trap2Y;
+    do {
+        trap2X = rand() % 5 + 1;
+        if(trap2X == 1) {
+            trap2Y = rand() % 4 + 2;
+        } else if(trap2X == 5) {
+            trap2Y = rand() % 4 + 1;
         } else {
-            bh = rand()%5+1;
+            trap2Y = rand() % 5 + 1;
         }
-        n[ah][bh] = 4;
-        
-        int ah2, bh2;
-        ah2 = rand()%5+1;
-        if(ah2==1) {
-            bh2 = rand()%4+2;
-        } else if(ah2==5) {
-            bh2 = rand()%4+1;
+    } while(trap2X == trap1X && trap2Y == trap1Y);
+    
+    game.map[trap2X][trap2Y] = TRAP;
+    game.traps.push_back({trap2X, trap2Y});
+}
+
+void printMap(const GameState& game) {
+    for(int i = 0; i < MAP_SIZE; i++) {
+        for(int j = 0; j < MAP_SIZE; j++) {
+            switch(game.map[i][j]) {
+                case PLAYER: cout << "人"; break;
+                case EMPTY: cout << "__"; break;
+                case GOAL: cout << "星"; break;
+                case WALL: cout << "墙"; break;
+                case TRAP: cout << "火"; break;
+                case DOOR: cout << "门"; break;
+                default: cout << "  "; break;
+            }
+        }
+        cout << endl;
+    }
+    
+    cout << endl << "当前关数：" << game.level << endl;
+    if(game.level == 5) {
+        cout << "PS：您可以输入ddd来右移三格，以此类推" << endl;
+    }
+}
+
+void processInput(GameState& game, char input) {
+    int newX = game.playerX;
+    int newY = game.playerY;
+    
+    switch(input) {
+        case 's': if(newX < 5) newX++; break;
+        case 'w': if(newX > 1) newX--; break;
+        case 'd': if(newY < 5) newY++; break;
+        case 'a': if(newY > 1) newY--; break;
+        default:
+            cout << "\n输" << endl;
+            system("pause");
+            exit(0);
+    }
+    
+    // 更新玩家位置
+    game.map[game.playerX][game.playerY] = EMPTY;
+    game.playerX = newX;
+    game.playerY = newY;
+    game.map[game.playerX][game.playerY] = PLAYER;
+}
+
+bool checkGameStatus(const GameState& game) {
+    // 检查胜利条件
+    if(game.playerX == game.goalX && game.playerY == game.goalY) {
+        cout << "\nvictory" << endl;
+        system("pause");
+        return false;
+    }
+    
+    // 检查陷阱条件
+    for(const auto& trap : game.traps) {
+        if(game.playerX == trap.first && game.playerY == trap.second) {
+            cout << "\nlose" << endl;
+            system("pause");
+            exit(0);
+        }
+    }
+    
+    // 检查特殊边界条件
+    if((game.playerX == 1 && game.playerY == 0) || (game.playerX == 0 && game.playerY == 1)) {
+        if(rand() % 2 == 1) {
+            cout << "\nvictory" << endl;
+            system("pause");
+            return false;
         } else {
-            bh2 = rand()%5+1;
+            cout << "\nlose" << endl;
+            system("pause");
+            exit(0);
         }
-        
-        while(ah==ah2 and bh==bh2) {
-            ah2 = rand()%5+1;
-            if(ah2==1) {
-                bh2 = rand()%4+2;
-            } else if(ah2==5) {
-                bh2 = rand()%4+1;
-            } else {
-                bh2 = rand()%5+1;
-            }
-        }
-        n[ah2][bh2] = 4;
-        
-        for(;;) {
-            n[a][b] = 1;
-            for(int i=0; i<=6; i++) {
-                for(int j=0; j<=6; j++) {
-                    if(n[i][j]==1) {
-                        cout<<"人";
-                    } else if(n[i][j]==0) {
-                        cout<<"__";
-                    } else if(n[i][j]==2) {
-                        cout<<"星"; 
-                    } else if(n[i][j]==3) {
-                        cout<<"墙";
-                    } else if(n[i][j]==4) {
-                        cout<<"火";
-                    } else {
-                        cout<<"门";
-                    } 
-                }
-                cout<<endl;
-            }
-            
-            cout<<endl;
-            cout<<"当前关数："<<gs<<endl;
-            if(gs==5) {
-                cout<<"PS：您可以输入ddd来右移三格，以此类推"<<endl;
-            }
-            
-            if(a==5 and b==5) {
-                cout<<"\nvictory"<<endl;
-                system("pause");
-                break; 
-            } else if((a==ah and b==bh) or (a==ah2 and b==bh2)) {
-                cout<<"\nlose"<<endl;
-                system("pause");
-                return 0;
-            }
-            
-            int gl;
-            gl = rand()%2;
-            
-            if((a==1 and b==0) or (a==0 and b==1)) {
-                if(gl==1) {
-                    cout<<"\nvictory"<<endl;
-                    system("pause");
-                    break;
-                } else {
-                    cout<<"\nlose"<<endl;
-                    system("pause");
-                    return 0;
-                }
-            }		
-            
-            char ru;
-            cout<<"请进行移动 ";
-            cin>>ru;
-            
-            if(ru=='s') {
-                if(a<5) {
-                    a++;
-                    n[a-1][b] = 0;
-                }
-            } else if(ru=='w') {
-                if(a>1) {
-                    a--;
-                    n[a+1][b] = 0;
-                } else if(a==1 and b==1) {
-                    a--;
-                    n[1][1] = 0;
-                }	
-            } else if(ru=='d') {
-                if(b<5) {
-                    b++;
-                    n[a][b-1] = 0;
-                }
-            } else if(ru=='a') {
-                if(b>1) {
-                    b--;
-                    n[a][b+1] = 0;
-                } else if(a==1 and b==1) {
-                    b--;
-                    n[1][1] = 0;
-                }
-            } else {
-                cout<<"\n输"<<endl;
-                system("pause");
-                return 0;
-            }
-            
-            system("cls");
-        }
-        
-        system("cls");
-    }	 
+    }
+    
+    return true;
+}
+
+void clearScreen() {
+    system("cls");
 }
